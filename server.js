6a0daf6a53fde6319468d24e17010a623ffca349
Venkata Ramesh Kotthapalli <vkotthap@uncc.edu>
2023-09-17 19:14:48 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs'); //to include the Node.js built-in file system module
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 60
-        },
-        {
-            title: 'Rent',
-            budget: 410
-        },
-        {
-            title: 'Grocery',
-            budget: 120
-        },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('budget-data.json', 'utf8', (err, data) => { //to read the content of the budget-data.json file
+        if (err) {
+            console.error('Error reading JSON file:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        
+        const budgetData = JSON.parse(data);
+        res.json(budgetData);
+    });
 });
 
 app.listen(port, () => {
